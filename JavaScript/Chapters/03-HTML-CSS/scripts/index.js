a821209b3a19c22e7f0434a24e2deff93cace41d
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2025-06-29 21:46:24 +0530

    javascript -project1 completed

---
+++ JavaScript/Chapters/03-HTML-CSS/scripts/index.js
@@ -0,0 +1,11 @@
+
+console.log('I am coming from index.js')
+console.error("This is a Error")
+console.warn("Warning Method")
+
+console.log('This is a Comment')
+/* This is a comment
+console.log('Welcome Screen')
+*/
+
+//console.log('Second Method')
